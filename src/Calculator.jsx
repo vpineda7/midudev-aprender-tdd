c69b6cd4781d092566fd7c75bfc49193b9c07d2e
Miguel Ángel Durán <miduga@gmail.com>
2024-08-16 00:02:28 +0200

    Fix lint files

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import { evaluate } from "mathjs";
+import { useState } from 'react'
+import { evaluate } from 'mathjs'
 
-export const operations = ["+", "-", "*", "/"];
-const equalSign = "=";
+export const operations = ['+', '-', '*', '/']
+const equalSign = '='
 
-export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
 
 export const Calculator = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
 
-  const createHandleClick = (op) => () => setValue(value.concat(op));
+  const createHandleClick = (op) => () => setValue(value.concat(op))
 
   return (
     <div>
       <h1>Calculator</h1>
       <input value={value} readOnly />
-      <div role="grid">
+      <div role='grid'>
         {rows.map((row, idx) => (
-          <div key={idx} role="row">
+          <div key={idx} role='row'>
             {row.map((number) => (
               <button onClick={createHandleClick(number)} key={number}>
                 {number}
@@ -35,5 +35,5 @@ export const Calculator = () => {
         <button onClick={() => setValue(evaluate(value))}>{equalSign}</button>
       </div>
     </div>
-  );
-};
+  )
+}
--- src/can-reconfigure.js
+++ src/can-reconfigure.js
@@ -1,24 +1,24 @@
 export const canReconfigure = (from, to) => {
-  if (typeof from !== "string") throw new Error("from must be a string");
-  if (typeof to !== "string") throw new Error("to must be a string");
+  if (typeof from !== 'string') throw new Error('from must be a string')
+  if (typeof to !== 'string') throw new Error('to must be a string')
 
-  const isSameLength = from.length === to.length;
-  if (!isSameLength) return false;
+  const isSameLength = from.length === to.length
+  if (!isSameLength) return false
 
-  const hasSameUniqueLetters = new Set(from).size === new Set(to).size;
-  if (!hasSameUniqueLetters) return false;
+  const hasSameUniqueLetters = new Set(from).size === new Set(to).size
+  if (!hasSameUniqueLetters) return false
 
-  const transformations = {};
+  const transformations = {}
 
   for (let i = 0; i < from.length; i++) {
-    const fromLetter = from[i];
-    const toLetter = to[i];
+    const fromLetter = from[i]
+    const toLetter = to[i]
 
-    const storeLetter = transformations[fromLetter];
-    if (storeLetter && storeLetter !== toLetter) return false;
+    const storeLetter = transformations[fromLetter]
+    if (storeLetter && storeLetter !== toLetter) return false
 
-    transformations[fromLetter] = toLetter;
+    transformations[fromLetter] = toLetter
   }
 
-  return true;
-};
+  return true
+}
